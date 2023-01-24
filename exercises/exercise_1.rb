0b730f74a0fd11b47334bd589bbaa34743d6522b
Hadley <sutherlandhadley@gmail.com>
2023-01-24 00:38:03 -0700

    exer 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+Burnaby = Store.new(annual_revenue: 300000, mens_apparel:true, womens_apparel:true)
+Richmond = Store.new(annual_revenue: 1260000, mens_apparel:false, womens_apparel:true)
+Gastown = Store.new(annual_revenue: 190000, mens_apparel:true, womens_apparel:false)
